85c239277f4fc1fdbf315951b889f72187b48051
meresti <zsolt.horvat@gmail.com>
2015-12-01 18:01:41 +0200

    card view prototype

--- ux/src/main/webapp/app/src/app.js
+++ ux/src/main/webapp/app/src/app.js
@@ -4,6 +4,7 @@ export class App {
         config.map([
             {route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome'},
             {route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Github Users'},
+            {route: 'links', name: 'links', moduleId: 'links', nav: true, title: 'Links'},
             {route: 'child-router', name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router'}
         ]);
 
